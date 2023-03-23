618e27e37b4779951762db5fae49c77bd2477658
Ryan Weispfenning <weisp014@gmail.com>
2023-03-23 11:27:24 -0500

    initial setup

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "shoppingCart" (
+  "id" serial primary key,
+  "name" varchar(80) not null,
+  "quantity" decimal not null,
+  "unit" varchar(20),
+  "purchased" boolean default false
+);
+
+INSERT INTO "shoppingCart" ("name", "quantity", "unit")
+VALUES ('donuts', 1, 'dozen'),
+('bananas', 1.5, 'bunch');
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
@@ -10,6 +10,8 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
+const cartRouter = require('./routes/cart.routerjs')
+app.use('/cart', cartRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
