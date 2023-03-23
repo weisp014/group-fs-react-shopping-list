bd1f9404ed501c98a35251ff1577b4dcb4f8eaad
Steven Gangl <steven.h.gangl@gmail.com>
2023-03-23 14:05:34 -0500

    fixed merge problem

--- server/routes/cart.router.js
+++ server/routes/cart.router.js
@@ -4,11 +4,32 @@ const pool = require('../modules/pool.js');
 
 // TODO all routes
 
+//POST route
+router.post('/', (req, res) => {
+  const newItem = req.body;
+  console.log('req.body:', newItem);
+  
+  const sqlText = `INSERT INTO "shoppingCart" ("name", "quantity", "unit") VALUES ($1, $2, $3)`;
 
-
-
-
-
-
-
-module.exports = router;
+  pool.query(sqlText, [newItem.name, newItem.quantity, newItem.unit])
+      .then((result) => {
+          res.sendStatus(201);
+      })
+      .catch((error) => {
+          console.log(`Error making database query ${sqlText}`, error);
+          res.sendStatus(500);
+      });
+});
+    
+// GET
+router.get('/', (req, res) => {
+    const sqlText = `SELECT * FROM "shoppingCart";`;
+    pool.query(sqlText)
+        .then( (result) => {
+            res.send(result.rows);
+        })
+        .catch( (err) => {
+            console.log(`Error making DB query ${sqlText}`, error);
+            res.sendStatus(500);
+        });
+});
